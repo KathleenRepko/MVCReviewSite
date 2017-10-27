92265741d61d521a2cc1f09ec79a2345c5b18220
KathleenRepko <kathleen.repko@gmail.com>
2017-10-27 13:44:23 -0400

    Added Review and Category models, and scaffolded controllers.

---
+++ MVCReviewSite/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    public class Category
+    {
+        //Web application administrator should establish a list of property types (Hotel, Restaurant, Attraction, etc.)
+        [Key]
+        public int CategoryID { get; set; }
+        [Display(Name ="Property Category (hotel, restaurant...")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Review> Review { get; set; }
+    }
+}
---
+++ MVCReviewSite/Models/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSite.Models
+{
+    public class Review
+    {
+        //Model holds the pertinent review data, and relates to Category and Rating models.
+        [Key]
+        public int ReviewID { get; set; }
+        [MaxLength(40), Display(Name ="Review Title")]
+        public string Title { get; set; }
+        [Display(Name ="Name of Property")]
+        public string PropertyName { get; set; }
+        [Display(Name ="Location of Property (City/State/Country)")]
+        public string PropertyLocation { get; set; }
+        [Display(Name ="Reviewer Name")]
+        public string Reviewer { get; set; }
+        [Display(Name ="Date Reviewed")]
+        public DateTime Date { get; set; }
+        [MaxLength(500), Display(Name ="Write Review:")]
+        public string Content { get; set; }
+
+        [ForeignKey("Category"), Display(Name ="Property Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
